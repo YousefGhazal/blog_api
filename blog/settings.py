4adc129e5a98835adea64ea6045db17fe1983dd3
Yousef <yousefghazal2412@gmail.com>
2023-02-05 22:50:02 +0200

    delete white_noise

--- blog/settings.py
+++ blog/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
     "api",
+    
     'ckeditor',
     'drf_yasg',
     'rest_framework_simplejwt',
@@ -57,7 +59,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "blog.urls"
